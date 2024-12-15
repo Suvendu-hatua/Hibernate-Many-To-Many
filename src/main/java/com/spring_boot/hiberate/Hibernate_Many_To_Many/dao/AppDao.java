328735a1ff377ed51a41f9a271969b562dd6359b
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-15 22:59:27 +0530

    Adding AppDao ,it's implementation class and also updating code

---
+++ src/main/java/com/spring_boot/hiberate/Hibernate_Many_To_Many/dao/AppDao.java
@@ -0,0 +1,14 @@
+package com.spring_boot.hiberate.Hibernate_Many_To_Many.dao;
+
+import com.spring_boot.hiberate.Hibernate_Many_To_Many.entity.Course;
+import com.spring_boot.hiberate.Hibernate_Many_To_Many.entity.Student;
+
+public interface AppDao {
+    public void addStudentWithCourse(Student student);
+    public void addCourse(Course course);
+    public void addStudent(Student student);
+    public Student findStudentWithFetchCourse(int id);
+    public Course findCourseWithFetchStudent(int id);
+    public void deleteStudentById(int id);
+    public void deleteCourseById(int id);
+}
---
+++ src/main/java/com/spring_boot/hiberate/Hibernate_Many_To_Many/dao/AppDaoImpl.java
@@ -0,0 +1,66 @@
+package com.spring_boot.hiberate.Hibernate_Many_To_Many.dao;
+
+import com.spring_boot.hiberate.Hibernate_Many_To_Many.entity.Course;
+import com.spring_boot.hiberate.Hibernate_Many_To_Many.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDaoImpl implements AppDao{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDaoImpl(EntityManager entityManager){
+        this.entityManager=entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void addStudentWithCourse(Student student) {
+        entityManager.persist(student);
+    }
+
+    @Override
+    @Transactional
+    public void addCourse(Course course) {
+        entityManager.persist(course);
+    }
+
+    @Override
+    @Transactional
+    public void addStudent(Student student) {
+        entityManager.persist(student);
+    }
+
+    @Override
+    public Student findStudentWithFetchCourse(int id) {
+        TypedQuery<Student>query =entityManager.createQuery("select s from Student s join fetch s.courses where s.id=:data",Student.class);
+        query.setParameter("data",id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Course findCourseWithFetchStudent(int id) {
+        TypedQuery<Course>query =entityManager.createQuery("select c from Course c join fetch c.students where c.id=:data",Course.class);
+        query.setParameter("data",id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    @Transactional
+    public void deleteStudentById(int id) {
+        Student student=entityManager.find(Student.class,id);
+        entityManager.remove(student);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCourseById(int id) {
+        Course course=entityManager.find(Course.class,id);
+        entityManager.remove(course);
+    }
+}
