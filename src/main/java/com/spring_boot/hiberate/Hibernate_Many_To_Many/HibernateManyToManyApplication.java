33a105c84b973d1377816bb618c7a0d0fc49ec7a
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-15 23:32:37 +0530

    Calling dao methods from main application.

--- src/main/java/com/spring_boot/hiberate/Hibernate_Many_To_Many/HibernateManyToManyApplication.java
+++ src/main/java/com/spring_boot/hiberate/Hibernate_Many_To_Many/HibernateManyToManyApplication.java
@@ -18,15 +18,46 @@ public class HibernateManyToManyApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDao dao){
 		return runner->{
-			addStudentWithCourses(dao);
+//			addStudentWithCourses(dao);
+//			addCourse(dao);
+//			addStudent(dao);
+//			assignCourseToStudent(dao);
+			deleteCourseById(dao);
 		};
+	}
+
+	private void deleteCourseById(AppDao dao) {
+		int courseId=1;
+		dao.deleteCourseById(courseId);
+		System.out.println("Done!!");
+	}
+
+	private void assignCourseToStudent(AppDao dao) {
+		int courseId=4;
+		int studentId=2;
+		System.out.println("Adding course to Student....");
+		dao.assignCourseToStudent(courseId,studentId);
+		System.out.println("Done!!!");
+	}
+
+	private void addStudent(AppDao dao) {
+		Student student=new Student("Sonali","Samanta","samanta@gmail.com");
+		System.out.println("Saving Student into DB....");
+		dao.addStudent(student);
+	}
+
+	private void addCourse(AppDao dao) {
+		Course course=new Course("Angular Course",3999.99f,20);
+		System.out.println("Saving course intoDB....");
+		dao.addCourse(course);
+		System.out.println("Done!!!");
 	}
 
 	private void addStudentWithCourses(AppDao dao) {
 		//creating student instance
 		Student student=new Student("Suvendu","Hatua","suvendu@gmail.com");
 		//creating course instances.
-		Course course1=new Course("Java Course",5999.99f,30);
+		Course course1=new Course("Angular Course",5999.99f,30);
 		Course course2=new Course("Computer Networks",3499.99f,25);
 		Course course3=new Course("System Design",9999.99f,30);
 		//Adding courses to student instance.
