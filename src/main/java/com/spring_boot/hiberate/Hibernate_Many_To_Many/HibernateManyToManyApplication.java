35e3af7b2b87504dc7324eb9e0fec4ebf6f5b540
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-15 20:39:29 +0530

    git init

---
+++ src/main/java/com/spring_boot/hiberate/Hibernate_Many_To_Many/HibernateManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.hiberate.Hibernate_Many_To_Many;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateManyToManyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot/hiberate/Hibernate_Many_To_Many/HibernateManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.hiberate.Hibernate_Many_To_Many;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
